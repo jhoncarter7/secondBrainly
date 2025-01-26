62e7e658da88c79337f809ca89b1fcbd2fbb66f1
nikhilQuadb <rainikhil044@gmail.com>
2025-01-26 23:42:05 +0530

    Add CORS middleware to enable cross-origin resource sharing

--- server/app.ts
+++ server/app.ts
@@ -7,6 +7,7 @@ import contentRouter from './routers/content.routes';
 import linkRouter from './routers/link.routes'
 import bodyParser from 'body-parser';
 import cookieParser from "cookie-parser";
+import cors from 'cors'
 dotenv.config(); 
  // Load environment variables before using them
 const app = express();
@@ -14,6 +15,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors())
 app.use('/api/v1', userAuthRouter)
 app.use('/api/v1/content', userVerify, contentRouter)
 app.use('/api/v1/link', userVerify, linkRouter)
